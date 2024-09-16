6d743c575b50aea7c5068226a6e8e381eb051e65
realwillianflecha <ramonflechaduarte@gmail.com>
2024-09-16 18:51:28 -0400

    Add files via upload

---
+++ imprimir_numeros_1_al_5_ciclo_for.py
@@ -0,0 +1,12 @@
+# Ciclo for en Python
+# Funciona mas para recorrer listas
+# recorrer una cadena
+for letra in 'Hola Mundo':
+    print(letra, end=' ')
+
+# Usando la funcion de range podemos crear una secuencia de valores
+#numeros = range(1, 6, 1)
+numeros = range(1, 6)
+print()
+for numero in numeros:
+    print(numero, end=' ')
