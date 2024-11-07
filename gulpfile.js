983a75186d2aa8ff59e0ba44019ccde2694bedcc
realwillianflecha <ramonflechaduarte@gmail.com>
2024-11-07 20:38:08 -0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ function css( done ) {
     //pasos: 1 - identificar archivo, 2 - Compilarla, 3 - Guardar el .css
     
     src ('src/scss/app.scss')
-        .pipe( sass() )
+        .pipe( sass({ outputStyle: 'expanded'}) )
         .pipe( dest('build/css') )
 
     done()
