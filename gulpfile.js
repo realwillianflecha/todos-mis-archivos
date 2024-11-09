4c066d0a850518fb7d3eab5339f40fda5d2d2cb4
realwillianflecha <ramonflechaduarte@gmail.com>
2024-11-08 23:09:28 -0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
-const { src, dest, watch } = require('gulp');
-const sass = require('gulp-sass')(require('sass'))
+const { src, dest, watch, series, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css( done ) {
     //compilar sass
     //pasos: 1 - identificar archivo, 2 - Compilarla, 3 - Guardar el .css
     
     src ('src/scss/app.scss')
-        .pipe( sass({ outputStyle: 'expanded'}) )
+        .pipe( sass() )
+        .pipe( postcss([ autoprefixer() ]) )
         .pipe( dest('build/css') )
 
     done()
@@ -17,5 +20,13 @@ function dev() {
     watch( 'src/scss/app.scss', css );
 }
 
+function tareaDefault() {
+    console.log('soy la tarea por default')
+}
+
 exports.css = css;
 exports.dev = dev;
+exports.default = series( css, dev );
+
+// series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente
+//parallel - Todas iniciain al mismo tiempo
