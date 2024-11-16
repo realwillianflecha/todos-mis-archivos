726ace1182238b21c0d929c20a0121157574190e
realwillianflecha <ramonflechaduarte@gmail.com>
2024-11-16 10:57:47 -0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 const { src, dest, watch, series, parallel } = require('gulp');
+
+// CSS y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+// Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function css( done ) {
     //compilar sass
@@ -15,18 +21,28 @@ function css( done ) {
     done()
 }
 
-function dev() {
-
-    watch( 'src/scss/app.scss', css );
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe( imagemin({optimizationLevel: 3 }) )
+        .pipe( dest( 'build/img' ) );
 }
 
-function tareaDefault() {
-    console.log('soy la tarea por default')
+function versionWebp() {
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( webp() )
+        .pipe( dest('build/img') )
+}
+
+function dev() {
+    watch( 'src/scss/**/*.scss', css );
+    watch( 'src/img/**/*', imagenes );
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series( css, dev );
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.default = series( imagenes, versionWebp, css, dev );
 
 // series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente
 //parallel - Todas iniciain al mismo tiempo
