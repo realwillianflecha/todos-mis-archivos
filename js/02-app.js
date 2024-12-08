f465ef7522530d90b2a0dc06f0398886a3c52109
realwillianflecha <ramonflechaduarte@gmail.com>
2024-12-08 17:04:14 -0300

    Add files via upload

---
+++ js/02-app.js
@@ -0,0 +1,14 @@
+const producto = 'Monitor 20 Pulgadas';
+
+console.log(producto);
+
+// Conocer la cantidad de letras de la cadena de caracteres
+console.log(producto.length);
+
+
+
+// console.log(producto.indexOf('Monitor'));
+
+
+console.log(producto.includes('Tablet'));
+console.log(producto.includes('Monitor'));
