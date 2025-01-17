96349aa1af885fecda415a22972c83831fc6e2cc
realwillianflecha <ramonflechaduarte@gmail.com>
2025-01-17 00:01:01 -0300

    Add files via upload

--- 13-DOM/js/11-scripts.js
+++ 13-DOM/js/11-scripts.js
@@ -6,10 +6,12 @@ btnFlotante.addEventListener('click', mostrarOcultarFooter);
 function mostrarOcultarFooter() {
     if(footer.classList.contains('activo')) {
         footer.classList.remove('activo')
-        btnFlotante.classList.remove('activo');
+        this.classList.remove('activo');
+        this.textContent = 'Idioma y Moneda';
     } else {
         footer.classList.add('activo');
-        btnFlotante.classList.add('activo');
+        this.classList.add('activo');
+        this.textContent = 'X Cerrar';
     }
     
 }
