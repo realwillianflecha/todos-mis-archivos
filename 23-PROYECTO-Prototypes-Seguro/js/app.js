08cd54ac619cd081d17330812de6596cf67c09a3
realwillianflecha <ramonflechaduarte@gmail.com>
2025-03-09 23:20:40 -0300

    Add files via upload

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -31,10 +31,23 @@ Seguro.prototype.cotizarSeguro = function () {
     }
 
     // Leer el año
+    const diferencia = new Date().getFullYear() - this.year;
 
-    // Cada año que la diferencia es mayor, el costo va a reducirse un 3% 
+    // Cada año que la diferencia es mayor, el costo va a reducirse un 3%
+    cantidad -= ((diferencia * 3) * cantidad) / 100;
 
- console.log(cantidad);
+    /*
+        Si el seguro es basico se multiplica por un 30% mas
+        Si el seguro es completo se multiplica por un 50% mas
+    */
+
+    if(this.tipo === 'basico') {
+        cantidad *= 1.30;
+    } else {
+        cantidad *= 1.50;
+    }
+
+    return cantidad;
 }
 
 
@@ -42,7 +55,7 @@ function UI() {}
 
 // Llena las opciones de los años
 UI.prototype.llenarOpciones = () => {
-    const max = new Date().getFullYear() - 5,
+    const max = new Date().getFullYear();
           min = max - 20;
 
     const selectYear = document.querySelector('#year');
@@ -79,6 +92,53 @@ UI.prototype.mostrarMensaje = (mensaje, tipo) => {
     }, 3000)
 }
 
+UI.prototype.mostraResultado = (seguro, total) => {
+    
+    const {marca, year, tipo} = seguro;
+
+    let textoMarca;
+
+    switch(marca) {
+        case '1':
+            textoMarca = 'Americano';
+            break;
+        case '2':
+            textoMarca = 'Asiatico';
+            break;
+        case '3':
+            textoMarca = 'Europeo';
+            break;
+
+
+        default:
+            break;
+    }
+
+    // Crear el resultado
+    const div = document.createElement('div');
+    div.classList.add('mt-10');
+
+    div.innerHTML = `
+        <p class="header">Tu Resumen</p>
+        <p class="font-bold">Marca: <span class="font-normal"> $ ${textoMarca} </span> </p>
+        <p class="font-bold">Año: <span class="font-normal"> $ ${year} </span> </p>
+        <p class="font-bold">Tipo: <span class="font-normal capitalize"> $ ${tipo} </span> </p>
+        <p class="font-bold">Total: <span class="font-normal"> $ ${total} </span> </p>
+    `;
+
+    const resultadoDiv = document.querySelector('#resultado');
+    
+    
+    // Mostrar el spinner
+    const spinner = document.querySelector('#cargando');
+    spinner.style.display = 'block';
+
+    setTimeout(() => {
+        spinner.style.display = 'none' // Se borra el spinner
+        resultadoDiv.appendChild(div); // Se muestra el resultado
+    }, 3000);
+}
+
 // Instanciar UI
 const ui = new UI();
 
@@ -110,12 +170,19 @@ function cotizarSeguro(e) {
         ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
         return;
     }
+
     ui.mostrarMensaje('Cotizando...', 'exito');
+
+    // Ocultar las cotizaciones previas
+    const resultados = document.querySelector('#resultado div');
+    if(resultados != null) {
+        resultados.remove();
+    }
 
     // Instanciar el seguro
     const seguro = new Seguro(marca, year, tipo);
-    seguro.cotizarSeguro();
+    const total = seguro.cotizarSeguro();
 
     // Utilizar el prototype que va a cotizar
-
+    ui.mostraResultado(seguro, total);
 }
