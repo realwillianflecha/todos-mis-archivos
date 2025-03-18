21d7d430133ad2f0dec3748cf425a6505c00b4dd
realwillianflecha <ramonflechaduarte@gmail.com>
2025-03-17 21:38:29 -0300

    Add files via upload

---
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -0,0 +1,45 @@
+// Variables y Selectores
+const formulario = document.querySelector('#agregar-gasto');
+const gastoListado = document.querySelector('#gastos ul');
+
+
+// Eventos
+
+eventListeners();
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto );
+}
+
+
+// Classes
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
+
+class UI {
+
+}
+
+// Instanciar
+const ui = new UI();
+let presupuesto;
+
+// Funciones
+
+function preguntarPresupuesto() {
+    const presupuestoUsuario = prompt('¿Cual es tu presupuesto?');
+
+    console.log( Number( presupuestoUsuario ) );
+
+    if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
+        window.location.reload();
+    }
+
+    // Presupuesto valido
+    presupuesto = new Presupuesto(presupuestoUsuario);
+
+}
