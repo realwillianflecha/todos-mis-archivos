f729ac92da253b2e3638c7dca495de5e9168aabd
realwillianflecha <ramonflechaduarte@gmail.com>
2025-03-24 21:38:17 -0400

    Add files via upload

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -4,6 +4,16 @@ const propietarioInput = document.querySelector('#propietario');
 const emailInput = document.querySelector('#email');
 const fechaInput = document.querySelector('#fecha');
 const sintomasInput = document.querySelector('#sintomas');
+const formulario = document.querySelector('#formulario-cita');
+
+// Eventos
+pacienteInput.addEventListener('change', datosCita);
+propietarioInput.addEventListener('change', datosCita);
+emailInput.addEventListener('change', datosCita);
+fechaInput.addEventListener('change', datosCita);
+sintomasInput.addEventListener('change', datosCita);
+
+formulario.addEventListener('submit', submitCita);
 
 // Objeto de cita
 const citaObj = {
@@ -14,7 +24,17 @@ const citaObj = {
     sintomas: ''
 }
 
-// Eventos
-pacienteInput.addEventListener('change', (e) => {
-    citaObj.paciente = e.target.value;
-})
+function datosCita(e) {
+    citaObj[e.target.name] = e.target.value;
+}
+
+function submitCita(e) {
+    e.preventDefault();
+
+    
+    if( Object.values(citaObj).some( valor => valor.trim() === '' )) {
+        console.log('Todos los campos son obligatorios');
+        return;
+    }
+
+}
