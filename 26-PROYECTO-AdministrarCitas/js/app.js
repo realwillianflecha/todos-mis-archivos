ecfa8a30297bd4d58aa514cc66a2890910621eee
realwillianflecha <ramonflechaduarte@gmail.com>
2025-03-25 22:00:49 -0400

    Add files via upload

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -33,8 +33,40 @@ function submitCita(e) {
 
     
     if( Object.values(citaObj).some( valor => valor.trim() === '' )) {
-        console.log('Todos los campos son obligatorios');
+        const notificacion = new Notificacion({
+            texto: 'Todos los campos son obligatorios',
+            tipo: 'error'
+        })
+        notificacion.mostrar();
         return;
     }
 
+}
+
+
+class Notificacion {
+    
+    constructor({texto, tipo}) {
+        this.texto = texto;
+        this.tipo = tipo;
+    }
+
+    mostrar() {
+        // Crear la notificacion
+        const alerta = document.createElement('DIV');
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'fond-bolt', 'text-sm');
+        
+        // Si es de tipo error, agrega una clase
+        this.tipo = 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
+        
+        // Mensaje de error
+        alerta.textContent = this.texto;
+
+        // Insertar en el DOM
+        formulario.parentElement.insertBefore(alerta, formulario);
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000)
+    }
 }
