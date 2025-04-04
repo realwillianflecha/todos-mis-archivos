81c57fe24aa6a567091bc1ea94e0f4377e6e5bb9
realwillianflecha <ramonflechaduarte@gmail.com>
2025-04-03 20:35:23 -0400

    Add files via upload

--- 31-PROYECTO-IndexedDB/js/app.js
+++ 31-PROYECTO-IndexedDB/js/app.js
@@ -109,8 +109,69 @@ class UI {
         // Leer el contenido de la base de datos
         const objectStore = DB.transaction('citas').objectStore('citas');
 
+        const total = objectStore.count();
+        console.log(total);
+
         objectStore.openCursor().onsuccess = function(e) {
-            console.log(e.target.result);
+            
+            const cursor = e.target.result;
+
+            if(cursor) {
+                const {mascota, propietario, telefono, fecha, hora, sintomas, id } = cursor.value;
+
+                const divCita = document.createElement('div');
+                divCita.classList.add('cita', 'p-3');
+                divCita.dataset.id = id;
+
+                // scRIPTING DE LOS ELEMENTOS...
+                const mascotaParrafo = document.createElement('h2');
+                mascotaParrafo.classList.add('card-title', 'font-weight-bolder');
+                mascotaParrafo.innerHTML = `${mascota}`;
+
+                const propietarioParrafo = document.createElement('p');
+                propietarioParrafo.innerHTML = `<span class="font-weight-bolder">Propietario: </span> ${propietario}`;
+
+                const telefonoParrafo = document.createElement('p');
+                telefonoParrafo.innerHTML = `<span class="font-weight-bolder">Teléfono: </span> ${telefono}`;
+
+                const fechaParrafo = document.createElement('p');
+                fechaParrafo.innerHTML = `<span class="font-weight-bolder">Fecha: </span> ${fecha}`;
+
+                const horaParrafo = document.createElement('p');
+                horaParrafo.innerHTML = `<span class="font-weight-bolder">Hora: </span> ${hora}`;
+
+                const sintomasParrafo = document.createElement('p');
+                sintomasParrafo.innerHTML = `<span class="font-weight-bolder">Síntomas: </span> ${sintomas}`;
+
+                // Agregar un botón de eliminar...
+                const btnEliminar = document.createElement('button');
+                btnEliminar.onclick = () => eliminarCita(id); // añade la opción de eliminar
+                btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+                btnEliminar.innerHTML = 'Eliminar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+
+                // Añade un botón de editar...
+                const btnEditar = document.createElement('button');
+                btnEditar.onclick = () => cargarEdicion(cita);
+
+                btnEditar.classList.add('btn', 'btn-info');
+                btnEditar.innerHTML = 'Editar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
+
+                // Agregar al HTML
+                divCita.appendChild(mascotaParrafo);
+                divCita.appendChild(propietarioParrafo);
+                divCita.appendChild(telefonoParrafo);
+                divCita.appendChild(fechaParrafo);
+                divCita.appendChild(horaParrafo);
+                divCita.appendChild(sintomasParrafo);
+                divCita.appendChild(btnEliminar)
+                divCita.appendChild(btnEditar)
+
+                contenedorCitas.appendChild(divCita);
+
+                // Ve al sigueiente elemento
+                cursor.continue();
+
+            }
         }
    }
 
@@ -131,7 +192,6 @@ class UI {
 
 
 const administrarCitas = new Citas();
-console.log(administrarCitas);
 const ui = new UI(administrarCitas);
 
 function nuevaCita(e) {
@@ -256,6 +316,9 @@ function crearDB() {
         console.log('BD creada');
 
         DB = crearDB.result;
+
+        // Mostrar citas al cargar (Pero indexedDB ya esta listo)
+        ui.imprimirCitas();
     }
 
     // Definir el schema
