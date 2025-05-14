2682fc2195bb009cc95b3a8442223233e35784df
realwillianflecha <ramonflechaduarte@gmail.com>
2025-05-13 21:53:45 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
 
 
 function App() {
+
+  // State
+  const [auth, setAuth] = useState(false)
+
+  console.log(auth)
 
   return (
     <> 
