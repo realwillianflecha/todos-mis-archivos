61c1979e25fcd17366ad278b2756178a6b14355f
realwillianflecha <ramonflechaduarte@gmail.com>
2025-05-16 23:42:36 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { use, useState } from 'react'
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
+import {db} from "./data/db"
 
 
 function App() {
+
+  const [data, setData] = useState(db)
 
 
   return (
@@ -15,14 +18,21 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            <Guitar/>
+            {data.map((guitar) => (
+                <Guitar
+                  key={guitar.id}
+                  guitar={guitar}
+
+                />  
+              )
+            )}
         </div>
     </main>
 
 
-    <footer class="bg-dark mt-5 py-5">
-        <div class="container-xl">
-            <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
         </div>
     </footer>
 
