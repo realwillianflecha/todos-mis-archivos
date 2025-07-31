b5aec42442514fbfdd91df9b132af6afc700cd17
realwillianflecha <ramonflechaduarte@gmail.com>
2025-07-31 00:15:07 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      backgroundImage : {
+        "header" : "url('/bg.jpg')"
+      }
+    },
+  },
   plugins: [],
 }
